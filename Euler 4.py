5380e5099cab8eecb6320862a05a43e7494ed732
hstringfield5455 <42746158+hstringfield5455@users.noreply.github.com>
2018-09-05 12:42:43 -0500

    Update and rename Euler 2.py to Euler 4.py

---
+++ Euler 4.py
@@ -0,0 +1,14 @@
+# Euler Fourth Problem
+# Holden Stringfield
+# This will find the largest palindrome 
+
+def main():
+  i = 1000
+  while i > 99:
+    i -= 1
+    j = 1000
+    while j > 99:
+      j -= 1
+      if str(i*j) == str(i*j)[::-1]:
+        print(i*j)
+        break
