5bf084025d5378b0fe9720aa53bb8132026ec099
hstringfield5455 <hstringfield5455@student.rcschools.net>
2018-09-10 17:41:55 -0500

    unknown changes

--- Euler 3.py
+++ Euler 3.py
@@ -3,24 +3,25 @@
 # This returns the largest prim factor of a number
 
 def main():
-  num = int(input("What is the number? "))
-  comp(num)
+    num = int(input("What is the number? "))
+    comp(num)
 
 def isPrime(n):
-  for i in range(2,int(n**0.5)+1):
-    if n%i==0:
-      return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
 
-  return True
+    return True
 
 def comp(num):
-  if isPrime(num) == True:
-    print("It is prime")
-    return
-  for i in range(num-1):
-    i += 1
-    if isPrime(i) and num % i == 0:
-      larg = i
-  print(larg)
+    if isPrime(num) == True:
+        print("It is prime")
+        return
+    else:
+        for i in range(num-1):
+            i += 1
+            if isPrime(i) and num % i == 0:
+                larg = i
+        print(larg)
 
 main()
